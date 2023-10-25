15eb62da14b73499491319cf33f4eb8a908206bd
chehinedh <chehine.dhemaied@esprit.tn>
2023-10-25 17:40:39 +0100

    connected to gateway + update dockerfile

--- src/main/java/com/example/forummicroservice/ForumMicroServiceApplication.java
+++ src/main/java/com/example/forummicroservice/ForumMicroServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.forummicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
-@EnableWebFluxSecurity
 public class ForumMicroServiceApplication {
 
 	public static void main(String[] args) {

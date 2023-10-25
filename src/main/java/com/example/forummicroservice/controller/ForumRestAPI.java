e47c761a21e45bbd51931e445580fbc49cf5afd4
chehinedh <chehine.dhemaied@esprit.tn>
2023-10-25 12:14:26 +0100

    conntected to gateway + update dockerfile

--- src/main/java/com/example/forummicroservice/controller/ForumRestAPI.java
+++ src/main/java/com/example/forummicroservice/controller/ForumRestAPI.java
@@ -11,7 +11,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/forums")
-public class ForumController {
+public class ForumRestAPI {
+
+    //private static final GrantedAuthority GOLD_CUSTOMER = new SimpleGrantedAuthority("gold");
+
+    //@GetMapping("/quotes/{symbol}")
+    //public Mono<Quote> getQuote(@PathVariable("symbol") String symbol,
+                               // BearerTokenAuthentication auth ) {
+
+       // Quote q = new Quote();
+       // q.setSymbol(symbol);
+       // if ( auth.getAuthorities().contains(GOLD_CUSTOMER)) {
+         //   q.setPrice(10.0);
+       // }
+        //else {
+         //   q.setPrice(12.0);
+        //}
+       // return Mono.just(q);
+    //}
 
     private String title="hello , im the updated forum microservice";
 
--- src/test/java/com/example/forummicroservice/ForumMicroServiceApplicationTests.java
+++ src/test/java/com/example/forummicroservice/ForumMicroServiceApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.forummicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootTest
+@EnableWebFluxSecurity
+
 class ForumMicroServiceApplicationTests {
 
 	@Test

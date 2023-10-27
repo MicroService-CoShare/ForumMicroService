01065129e51e27459a29c9bf6cdd004a7485cb86
chehinedh <chehine.dhemaied@esprit.tn>
2023-10-27 08:42:57 +0100

    fix a bug

--- src/main/java/com/example/forummicroservice/controller/ForumRestAPI.java
+++ src/main/java/com/example/forummicroservice/controller/ForumRestAPI.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
-@RequestMapping("/api/forums")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE ,RequestMethod.OPTIONS})
 @RestController
 public class ForumRestAPI {
